89a5c01758c28e0acd7187a2cfeca2bf92bc6e8e
Louis M <louisose@hotmail.com>
2015-01-24 18:03:58 -0400

    Added gemfile to repository. Ignoring sqlite files.

--- db/schema.rb
+++ db/schema.rb
@@ -75,8 +75,8 @@ ActiveRecord::Schema.define(version: 20150120160704) do
 
   create_table "taxis", force: :cascade do |t|
     t.string   "name"
-    t.decimal  "latitude"
-    t.decimal  "longitude"
+    t.float    "latitude"
+    t.float    "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "available"
